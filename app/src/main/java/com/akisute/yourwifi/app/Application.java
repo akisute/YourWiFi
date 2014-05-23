e0cd658716b7107234d92f869b3baa154291bf23
Ono Masashi <masashi.ono@linecorp.com>
2014-05-23 19:46:39 +0900

    Integrated Parse.com SDK.

--- app/src/main/java/com/akisute/yourwifi/app/Application.java
+++ app/src/main/java/com/akisute/yourwifi/app/Application.java
@@ -2,8 +2,16 @@ package com.akisute.yourwifi.app;
 
 import com.akisute.android.daggered.DaggeredApplication;
 import com.akisute.yourwifi.app.dagger.ApplicationModule;
+import com.parse.Parse;
 
 public class Application extends DaggeredApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "QDqla70qFIIfsls2HQRo1qMZM2wCGTkwjlZImaYb", "u64kUPWaPcvikkVzVWwWag6uvpgAPONF3HI8RHjk");
+    }
+
     @Override
     protected Object[] getModules() {
         return new Object[]{
