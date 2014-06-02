fa89e10b05c604f908fa80365231d1a1d3bb8c52
Ono Masashi <masashi.ono@linecorp.com>
2014-06-02 19:43:21 +0900

    Added login/logout menu.

--- app/src/main/java/com/akisute/yourwifi/app/SettingsActivity.java
+++ app/src/main/java/com/akisute/yourwifi/app/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.akisute.yourwifi.app;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.akisute.android.daggered.DaggeredActivity;
 
@@ -19,5 +21,34 @@ public class SettingsActivity extends DaggeredActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         setTitle(R.string.activity_settings_title);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem loginItem = menu.findItem(R.id.action_login);
+        MenuItem logoutItem = menu.findItem(R.id.action_logout);
+
+        // TODO: login state check
+        loginItem.setVisible(true);
+        logoutItem.setVisible(false);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                return true;
+            case R.id.action_logout:
+                return true;
+        }
+        return false;
     }
 }
